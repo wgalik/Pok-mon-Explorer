780b68588959efdee164971660d8c04a44df14eb
Wojtek Galik <w.galik@poczta.fm>
2025-12-02 10:01:27 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,13 @@
 import logoBall from "./assets/poke-ball.png";
 
 import { ref, computed } from "vue";
+import FormComponent from "./components/FormComponent.vue";
 import ButtonComponent from "./components/ButtonComponent.vue";
 import PokemonCard from "./components/PokemonCard.vue";
 
 const limit = ref(6);
 const pokemonList = ref([]);
 const gotData = ref(false);
-const text = defineModel();
-
-const list = computed(() => pokemonList.value.slice(0, limit.value));
-
-const getData = (search) => {
-  const API = `https://pokeapi.co/api/v2/type/${search}`;
-  pokemonList.value = [];
-  fetch(API)
-    .then((response) => {
-      if (response.ok) {
-        return response;
-      }
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      pokemonList.value = data.pokemon;
-      gotData.value = true;
-    });
-};
 
 const typeColors = {
   normal: "#A8A77A",
@@ -49,14 +31,34 @@ const typeColors = {
   fairy: "#D685AD",
 };
 
+const list = computed(() => pokemonList.value.slice(0, limit.value));
+
+const getData = (search) => {
+  const API = `https://pokeapi.co/api/v2/type/${search}`;
+  pokemonList.value = [];
+  fetch(API)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      pokemonList.value = data.pokemon;
+      gotData.value = true;
+    });
+};
+
 const buttonHandler = (e) => {
   e.target.classList.toggle("active");
+  if (!e.target.classList.contains("active")) return;
   const search = e.target.id;
   getData(search);
 };
 
-const submit = () => {
-  const search = text.value;
+const submit = (e, text) => {
+  if (!text) return;
+  const search = text;
   getData(search);
 };
 
@@ -69,50 +71,39 @@ const loadMore = () => (limit.value += 12);
       <img :src="logoBall" class="w-10 aspect-square inline-block align-top" />
       Pokémon Explorer
     </h1>
-    <form
-      @submit.prevent="submit"
-      class="w-full flex justify-center items-center"
-    >
-      <input
-        class="block max-w-150 w-full bg-white/5 h-10 p-4 rounded-full text-white"
-        @keyup.enter="submit"
-        type="search"
-        name=""
-        placeholder="Search Pokémon..."
-        v-model.trim="text"
-      />
-    </form>
+    <FormComponent @submit="submit" />
   </header>
+  <main class="w-full grid gap-4">
+    <section class="w-full flex justify-center items-center flex-wrap gap-4">
+      <ButtonComponent
+        type="button"
+        v-for="(value, key) in typeColors"
+        :key="value"
+        :id="key"
+        :style="{ backgroundColor: value }"
+        :text="key"
+        @click="buttonHandler"
+      />
+    </section>
 
-  <section class="w-full flex justify-center items-center flex-wrap gap-4">
-    <ButtonComponent
+    <section v-if="gotData" class="w-full grid grid-cols-3 gap-4">
+      <PokemonCard
+        v-for="pokemon in list"
+        :name="pokemon.pokemon.name"
+        :url="pokemon.pokemon.url"
+        :key="pokemon.pokemon.name"
+        :typeColors
+      />
+    </section>
+    <button
+      v-if="gotData"
       type="button"
-      v-for="(value, key) in typeColors"
-      :key="value"
-      :id="key"
-      :style="{ backgroundColor: value }"
-      :text="key"
-      @click="buttonHandler"
-    />
-  </section>
-
-  <section v-if="gotData" class="w-full grid grid-cols-3 gap-4">
-    <PokemonCard
-      v-for="pokemon in list"
-      :name="pokemon.pokemon.name"
-      :url="pokemon.pokemon.url"
-      :key="pokemon.pokemon.name"
-      :typeColors
-    />
-  </section>
-  <button
-    v-if="gotData"
-    type="button"
-    @click="loadMore"
-    class="grid w-40 px-4 font-bold text-white text-lg rounded-full border-2 bg-blue-500 border-[#121212] hover:bg-white hover:text-black active:outline-2 transition duration-300 ease-in-out cursor-pointer justify-self-center"
-  >
-    Load More
-  </button>
+      @click="loadMore"
+      class="grid w-40 px-4 font-bold text-white text-lg rounded-full border-2 bg-blue-500 border-[#121212] hover:bg-white hover:text-black active:outline-2 transition duration-300 ease-in-out cursor-pointer justify-self-center"
+    >
+      Load More
+    </button>
+  </main>
 </template>
 <style scoped>
 .active {
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+const text = defineModel();
+const emit = defineEmits(["submit"]);
+
+const submit = (event) => {
+  emit("submit", event, text.value);
+};
+</script>
+<template>
+  <form
+    class="w-full flex justify-center items-center"
+    @submit.prevent.enter="submit"
+  >
+    <input
+      class="block max-w-150 w-full bg-white/5 h-10 p-4 rounded-full text-white"
+      type="search"
+      name=""
+      placeholder="Search Pokémon..."
+      v-model.trim="text"
+    />
+  </form>
+</template>
+
+<style scoped></style>
